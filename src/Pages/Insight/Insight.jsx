821fb62523ab6b6e9fce82288c06d47dff27cf12
Errytagedesign <errytage@gmail.com>
2023-12-01 11:31:31 +0100

    Add insigth page

--- src/Pages/Insight/Insight.jsx
+++ src/Pages/Insight/Insight.jsx
@@ -1,8 +1,161 @@
 import React from 'react';
 import './Insight.scss';
+import { Link } from 'react-router-dom';
+import { PiCaretRightBold } from 'react-icons/pi';
+import home from '../../assets/home.png';
+import properties from '../../assets/properties.png';
+import { RiCheckboxBlankFill } from 'react-icons/ri';
+
+import { BsHouseFill, BsThreeDotsVertical } from 'react-icons/bs';
+import { ProgressBar } from 'react-bootstrap';
 
 function Insight() {
-  return <main className='insight'>Insight</main>;
+  return (
+    <main className='insight d-flex flex-column '>
+      <section className=''>
+        <h1 className='mb-5'> Rental Portfolio</h1>
+        <article className='d-flex flex-column flex-lg-row justify-content-between mb-4 gap-3'>
+          <section className='col-12 flex-fill rentalOverview card d-flex flex-column justify-content-between'>
+            <div className='d-flex justify-content-between'>
+              <div className='col-8'>
+                <h3>Rental Overview</h3>
+                <p>
+                  Show overview from Nov 2021 - Dec 2021.{' '}
+                  <span>
+                    {' '}
+                    <Link className='viewMore'>
+                      {' '}
+                      Detailed Stats <PiCaretRightBold />{' '}
+                    </Link>{' '}
+                  </span>{' '}
+                </p>
+              </div>
+              <div className='col-3 d-flex flex-column'>
+                <div>
+                  <input type='date' name='date' className='form-control' />
+                </div>
+              </div>
+            </div>
+
+            <article className='d-flex flex-column flex-lg-row justify-content-between mt-4'>
+              <div className='col-12 col-lg-4 listingUnit card '>
+                <figure className='col-3'>
+                  <img src={home} alt='' />
+                </figure>
+
+                <p className='my-3'> Listing Units</p>
+                <div className='d-flex gap-2 border-bottom pb-3'>
+                  <h4> 78</h4>
+                  <span className='greenBg'> +2.8%</span>{' '}
+                </div>
+
+                <p className='house mt-3'>
+                  {' '}
+                  <BsHouseFill className='me-1' />
+                  15 Available
+                </p>
+              </div>
+              <div className='col-12 col-lg-7 rentsData card mt-4 mt-lg-0'>
+                <h5> Rents</h5>
+
+                <div className='d-flex justify-content-between'>
+                  <div>
+                    <h4>4</h4>
+                    <p> Due this month</p>
+                  </div>
+                  <div>
+                    <div className='d-flex gap-1'>
+                      <h4>16</h4>
+                      <span className='purpleBg'>70.96%</span>
+                    </div>
+
+                    <p> Occupied this month</p>
+                  </div>
+                </div>
+
+                <hr className='my-3' />
+                <div className='d-flex justify-content-between'>
+                  <div>
+                    <h4 style={{ color: '#F06565' }}>15</h4>
+                    <p> Units Left</p>
+                  </div>
+                  <div>
+                    <h4>2</h4>
+
+                    <p> In Draft Unlisted</p>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </section>
+          <section className='col-12 col-lg-5 card propertiesOverview'>
+            <div className='d-flex justify-content-between'>
+              <div className='d-flex gap-2 col-6'>
+                <figure className='col-3'>
+                  <img src={properties} alt='' />
+                </figure>
+
+                <div>
+                  <h4>78</h4>
+                  <p> Properties </p>
+                </div>
+              </div>
+
+              <div className='d-flex flex-column justify-content-between align-items-end col-6'>
+                <BsThreeDotsVertical />
+                <span>
+                  {' '}
+                  <Link className='viewMore'>
+                    {' '}
+                    See all properties
+                    <PiCaretRightBold />{' '}
+                  </Link>{' '}
+                </span>{' '}
+              </div>
+            </div>
+
+            <ul className='propList d-flex flex-wrap gap-1 mt-3'>
+              {[
+                { id: 1, title: '22', sub: 'Vacant' },
+                { id: 2, title: '39', sub: 'Occupied' },
+                { id: 3, title: '15', sub: 'Unlisted' },
+                { id: 4, title: '2', sub: 'Unavailable' },
+              ].map(({ id, title, sub }) => (
+                <li key={id} className='text-center flex-fill'>
+                  <h4>{title}</h4>
+                  <p>{sub} </p>
+                </li>
+              ))}
+            </ul>
+
+            <div className='propProgress mt-2'>
+              <h3>Property listed </h3>
+
+              <ProgressBar now={68} className='my-3' />
+              <ProgressBar now={32} className='listed' />
+
+              <div className='d-flex gap-5 mt-3'>
+                <div className='d-flex gap-2 align-items-center'>
+                  <h6>
+                    <RiCheckboxBlankFill color='#5F259F' /> 68%{' '}
+                  </h6>{' '}
+                  <span> Occupied</span>
+                </div>
+                <div className='d-flex gap-2 align-items-center'>
+                  <h6>
+                    {' '}
+                    <RiCheckboxBlankFill color='#100a55' />
+                    32%{' '}
+                  </h6>{' '}
+                  <span> Vacant</span>
+                </div>
+              </div>
+            </div>
+          </section>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default Insight;
